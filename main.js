4a2aa0f81fe3bfdaa8bb0c343a922900b3ae2e06
= <--replace-all>
2020-09-25 11:13:58 +0100

    Task 3 complete, prompt and alert added

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ let computerMove = "paper";
 // if playerMove = computerMove then draw
 // if playerMove = Scissors && computerMove = Paper then Player wins
 
-function getWinner (playerMove, computerMove){ if(playerMove === computerMove) {
-    return ("0")
-} else if (playerMove === "rock" && computerMove === "scissors") {
-    return ("1")
-} else if (playerMove === "scissors" && computerMove === "paper") {
-    return ("1")
-} else if (playerMove === "paper" && computerMove === "rock") {
-    return ("1")
-} else {
-   return ("-1")
-}
+function getWinner (playerMove, computerMove){
+    
+    playerMove = prompt("3... 2... 1...")
+
+    if(playerMove === computerMove) {
+        alert("0")
+    } else if (playerMove === "rock" && computerMove === "scissors") {
+        alert("1")
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        alert("1")
+    } else if (playerMove === "paper" && computerMove === "rock") {
+        alert("1")
+    } else {
+        alert("-1")
+    }
 
 }
-let result = getWinner("scissors", "scissors");
-console.log(result);
+getWinner(playerMove,computerMove)
+//console.log(result);
+
+//Using `prompt`, get a user inputted value for the player move.
+//Then call your function with that value and the hard coded computer move. Display the result using `alert`.
